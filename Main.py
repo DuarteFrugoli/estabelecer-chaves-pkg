c37044aafa7de240026853d8d9be9a43ff3db11a
Pedro Henrique Duarte Frugoli <pedro.frugoli@ges.inatel.br>
2025-07-28 17:26:59 -0300

    organização de código, comentários e documentação

--- Main.py
+++ Main.py
@@ -15,7 +15,7 @@ from util import *
 # canal_rayleigh_1, canal_rayleigh_2: Vetores de canal Rayleigh para simulação (ganho de canal)
 # tabela: Tabela de códigos gerada
 # tamanho_espaco_amostral: Tamanho do espaço amostral para geração da tabela de códigos
-# canais: Dicionário com instâncias dos cenários de canal
+# canais: Dicionário com instâncias dos cenários de canal (possui apenas um cenário e será removido)
 # porcentagens_acerto: Lista de porcentagens_acerto de acerto para cada cenário
 # execution_time: Tempo total de execução da simulação
 # escala_rayleigh: Parâmetro de escala para a distribuição Rayleigh (sigma)
@@ -26,33 +26,6 @@ rayleigh_params = [0.5, 1.0, 2.0] # parâmetros Rayleigh para teste
 snr_db_range = np.linspace(-10, 30, 18) # 10 valores de SNR de -10 a 30 dB
 variancias_ruido = potencia_sinal / (10 ** (snr_db_range / 10))
 
-def inicializa_variaveis(tamanho_cadeia_bits):
-    """Inicializa as variáveis globais para os cenários."""
-    palavra_informacao = [random.randint(0, 1) for _ in range(tamanho_cadeia_bits)]  # palavra_informacao: Palavra de informação aleatória
-    canal_rayleigh_1 = np.random.rayleigh(escala_rayleigh, tamanho_cadeia_bits)               # canal_rayleigh_1: Vetor de canal Rayleigh
-    canal_rayleigh_2 = np.random.rayleigh(escala_rayleigh, tamanho_cadeia_bits)               # canal_rayleigh_2: Vetor de canal Rayleigh
-    tamanho_espaco_amostral = None if tamanho_cadeia_bits <= 15 else solicita_entrada(
-        "Entre com tamanho do espaço amostral: ", int, lambda v: v > 0
-    )
-    code_generator = CodeGenerator(tamanho_cadeia_bits)
-    tabela = code_generator.generate_code_table(tamanho_espaco_amostral)  # tabela: Tabela de códigos gerada
-
-    return palavra_informacao, canal_rayleigh_1, canal_rayleigh_2, tabela, tamanho_espaco_amostral
-
-
-def inicializa_canais(media, variancia, quantidade_de_testes):
-    """Inicializa os canais de comunicação."""
-    return {
-        "alto_ruido_rayleigh": AltoRuidoCanalRayleigh(media, variancia, quantidade_de_testes),
-    }
-
-def coletar_porcentagens(canais, palavra_informacao, canal_rayleigh_1, canal_rayleigh_2, tamanho_espaco_amostral, tabela, tamanho_cadeia_bits):
-    """Coleta as porcentagens_acerto de acertos para cada cenário."""
-    return [
-        canais["alto_ruido_rayleigh"].cenario(palavra_informacao, canal_rayleigh_1, canal_rayleigh_2, tamanho_espaco_amostral, tabela, tamanho_cadeia_bits),
-    ]
-
-
 # Marca o tempo inicial
 start_time = time.time()
 
@@ -62,11 +35,19 @@ quantidade_de_testes = solicita_entrada("Entre com a quantidade de testes: ", in
 tamanho_cadeia_bits = solicita_entrada("Entre com o tamanho da cadeia de Bits (7, 15, 127, 255): ", int, lambda v: v in {7, 15, 127, 255})
 
 # Inicializa variáveis e canais
-palavra_informacao, canal_rayleigh_1, canal_rayleigh_2, tabela, tamanho_espaco_amostral = inicializa_variaveis(tamanho_cadeia_bits)
-canais = inicializa_canais(media=0.5, variancia=1.5, quantidade_de_testes=quantidade_de_testes)
+palavra_informacao = [random.randint(0, 1) for _ in range(tamanho_cadeia_bits)]  # palavra_informacao: Palavra de informação aleatória
+canal_rayleigh_1 = np.random.rayleigh(escala_rayleigh, tamanho_cadeia_bits)               # canal_rayleigh_1: Vetor de canal Rayleigh
+canal_rayleigh_2 = np.random.rayleigh(escala_rayleigh, tamanho_cadeia_bits)               # canal_rayleigh_2: Vetor de canal Rayleigh
+tamanho_espaco_amostral = None if tamanho_cadeia_bits <= 15 else solicita_entrada(
+    "Entre com tamanho do espaço amostral: ", int, lambda v: v > 0
+)
+code_generator = CodeGenerator(tamanho_cadeia_bits)
+tabela = code_generator.generate_code_table(tamanho_espaco_amostral)  # tabela: Tabela de códigos gerada
+
+canais = AltoRuidoCanalRayleigh(media=0.5, variancia=1.5, ntestes=quantidade_de_testes) # TODO: remover dicionário de canais, pois só possui um cenário
 
 # Coleta as porcentagens_acerto de acertos
-porcentagens_acerto = coletar_porcentagens(canais, palavra_informacao, canal_rayleigh_1, canal_rayleigh_2, tamanho_espaco_amostral, tabela, tamanho_cadeia_bits)
+porcentagens_acerto = AltoRuidoCanalRayleigh.cenario(canais, palavra_informacao, canal_rayleigh_1, canal_rayleigh_2, tamanho_espaco_amostral, tabela, tamanho_cadeia_bits)
 
 # Plota os resultados
 Plotagem().plota(porcentagens_acerto, len(palavra_informacao))
@@ -76,6 +57,10 @@ execution_time = time.time() - start_time
 hours, rem = divmod(execution_time, 3600)
 minutes, seconds = divmod(rem, 60)
 print(f"Tempo de execução da simulação: {int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}")
+
+# TODO: a partir daqui variáveis mudam e o gráfico gerado em plotkar vai ser diferente, é preciso consertar isso para tudo funcionar junto.
+# em outras palavras, a parte de cima tem variáveis diferente da de baixo e isso é um problema.
+# agora que tudo está com um nome melhor dá para saber o que é o que e onde deve ser colocado.
 
 # Gera e plota os resultados para diferentes parâmetros Rayleigh
 for rayleigh_param in rayleigh_params:
---
+++ NOTES.md
@@ -0,0 +1,15 @@
+eu estou tentando muito simplificar as coisas, não precisam ser classes, João pelo amor de Deus cara.
+
+vou primeiro tirar as funções de inicializar pq n precisa.
+
+depois de ajeitar tudo que for necessário vou ter certeza de que nenhuma variável está sendo criada 2 vezes ou se perdendo no meio do caminho.
+
+eu encontrei variáveis sendo inicializadas em chamada de função, isso é um ultraje. Eu estou ficando maluco.
+
+Qual o caminho atual de informações do código:
+1. Main
+    1. variáveis iniciais
+    2. entradas
+    3. inicializa_canais (descessário e ainda por cima tem definição de variáveis nesse lugar)
+
+a última coisa que eu fiz 28/07 foi
